a5862df4973e9b6d0675ff19037db27cef7a33c0
Andrey Pechennikov <ap@devandy.me>
2022-11-19 20:41:39 +0300

    Добавил интерфейсы категорий, тегов и комментариев

--- taskforce/libs/shared-types/src/lib/task.interface.ts
+++ taskforce/libs/shared-types/src/lib/task.interface.ts
@@ -1,10 +1,14 @@
+import {Category} from './category.interface';
+import {Tag} from './tag.interface';
+
 export interface Task {
+  id: number;
   title: string;
   description: string;
-  category: string;
+  category: Category;
   price?: number;
   deadline?: Date;
   preview?: string;
   address?: string;
-  tags?: [];
+  tags?: Tag[];
 }
