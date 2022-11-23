14f824107aeaa7050fa82682ba45a54a812f9678
Andrey Pechennikov <ap@devandy.me>
2022-11-23 22:38:44 +0300

    Добавил подключение к базе данных

--- taskforce/apps/users/src/app/app.module.ts
+++ taskforce/apps/users/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
 import { ENV_FILE_PATH } from './app.constant';
 import databaseConfig from '../config/database.config';
 import { validateEnvironments } from './env.validation';
+import { getMongoDbConfig } from '../config/mongodb.config';
 
 @Module({
   imports: [
@@ -15,6 +17,9 @@ import { validateEnvironments } from './env.validation';
       load: [databaseConfig],
       validate: validateEnvironments,
     }),
+    MongooseModule.forRootAsync(
+      getMongoDbConfig()
+    ),
     AuthModule
   ],
   controllers: [],
--- taskforce/libs/core/src/lib/helpers.ts
+++ taskforce/libs/core/src/lib/helpers.ts
@@ -1,5 +1,14 @@
 import {ClassConstructor, plainToInstance} from 'class-transformer';
 
-export function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
+function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
   return plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
+}
+
+function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
+
+export {
+  fillObject,
+  getMongoConnectionString
 }
