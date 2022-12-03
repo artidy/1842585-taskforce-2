980f496003f123e1dc4da87e2ce79198366d1d34
Andrey Pechennikov <ap@devandy.me>
2022-12-03 20:36:28 +0300

    Рефакторинг интерфейсов для моделей проекта tasks

---
+++ taskforce/libs/shared-types/src/lib/candidate.interface.ts
@@ -0,0 +1,5 @@
+export interface Candidate {
+  id: number;
+  performerId: string;
+  taskId: number;
+}
--- taskforce/libs/shared-types/src/lib/task.interface.ts
+++ taskforce/libs/shared-types/src/lib/task.interface.ts
@@ -1,14 +1,23 @@
-import {Category} from './category.interface';
-import {Tag} from './tag.interface';
+import { Category } from './category.interface';
+import { Tag } from './tag.interface';
+import { Comment } from './comment.interface';
+import { Candidate } from './candidate.interface';
+import { Status } from './status.interface';
 
 export interface Task {
   id: number;
   title: string;
   description: string;
   category: Category;
-  price?: number;
+  budget?: number;
   deadline?: Date;
   preview?: string;
   address?: string;
   tags?: Tag[];
+  customerId: string;
+  performerId?: string;
+  candidates: Candidate[];
+  comments: Comment[];
+  status: Status;
+  createdAt: Date;
 }
